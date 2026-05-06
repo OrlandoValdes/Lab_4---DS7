e9e8a1382e1daf78aa55e911bf2737e224c9ff89
OrlandoValdes <orlando.valdes2@utp.ac.pa>
2026-05-05 22:50:35 -0500

    Enhance index.php with comments for clarity
    Added comments to clarify the purpose of imports and object instantiation.

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
 
-use App\User;
-use Database\Models\ProductModel;
+use App\User; // Aquí importamos la clase User del namespace App
+use Database\Models\ProductModel; // Aquí importamos la clase ProductModel del namespace Database/Models
 
-require 'vendor/autoload.php';
+require 'vendor/autoload.php'; //Solicita el archivo autoload.php en la carpeta vendor que debe ser generada con composer install
 
-$user= new User;
-echo $user->getName();
+$user= new User; //Se crea instancia de la clase User
+echo $user->getName(); //Imprime el Nombre
 echo "\n";
 
-$product= new ProductModel;
-echo $product->getId();
+$product= new ProductModel; //Se crea instancia de la clase ProductModel
+echo $product->getId(); //Imprime el ID
 
 echo "\n";
 ?>
